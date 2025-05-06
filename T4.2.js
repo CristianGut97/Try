dadeace30b245daa6e8df4f93b471d0f37445a8f
ChrisG <chisven97@gmail.com>
2025-05-06 18:53:37 -0500

    terminacion de taller

--- T4.2.js
+++ T4.2.js
@@ -1,5 +1,3 @@
-let numero;
-
 while (true) {
   numero = parseFloat(prompt("Escribe una calificación entre 0 y 100:"));
   
@@ -35,6 +33,7 @@ console.log(esPar);
 /--
 
 let monto;
+let descuento = 0;
 
 while(true){
     monto = parseFloat(prompt("Ingresa el monto "));
@@ -44,4 +43,29 @@ while(true){
         console.log("Ingresa un monto valido.");
     }
     
-    const descuento = monto>= 100 ? 10 : 
+    if (monto >= 100){
+        descuento = 0.1;
+        console.log("Se te aplica un ", descuento*100, "% de descuento");
+    }else console.log("no se aplica descuento");
+    
+    
+    
+    console.log("El monto final es: ", monto-(monto*descuento));
+
+    /--
+
+while(true){
+    let ingresado;
+    let numeroRandom = Math.floor(Math.random() * 10)+1;
+    while(true){
+    ingresado = parseInt(prompt("Ingresa un numero del 1 al 10: "));
+    if (!isNaN(ingresado) && ingresado >=1 && ingresado <=10){
+        break;
+    } console.log("Solo se puede ingresar un numero del 1 al 10");
+    }
+if (numeroRandom === ingresado) {
+    console.log("Felicidades! El numero era: ", numeroRandom);
+    break;
+} console.log("Lo siento, el numero era ", numeroRandom);
+}
+
